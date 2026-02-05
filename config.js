9998fa5381af43544acf29b7d5b75d93290b5b09
Abhi Raj <abhiraj27092009@gmail.com>
2026-02-06 02:22:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 {
     "proxy_ip": "34.93.238.161",
-    "proxy_port": "8083",
-    "cert_url": "https://raw.githubusercontent.com/abhirajdeveloperedu/bypass/refs/heads/main/free.cer.crt",
+    "proxy_port": "8082",
+    "cert_url": "https://raw.githubusercontent.com/abhirajdeveloperedu/bypass/refs/heads/main/paid.cer.crt",
     "message": "NineX v3.0 - Updated!",
     "enabled": true
 }
